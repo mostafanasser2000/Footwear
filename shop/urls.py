a47f7ad5b39e549998e82bcb22e0fd7a2f4f34d7
mostafanasser2000 <mustafanasser503@gmail.com>
2024-05-09 22:00:07 +0300

    create core application

--- shop/urls.py
+++ shop/urls.py
@@ -21,5 +21,5 @@ from django.urls import path, include
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("accounts/", include("allauth.urls")),
-    path("", include("users.urls")),
+    path("", include("core.urls")),
 ]
