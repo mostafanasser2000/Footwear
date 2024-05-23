0d642b3c80cc4ba8b5a1349a14367d5245994059
mostafanasser2000 <mustafanasser503@gmail.com>
2024-05-23 20:53:33 +0300

    add cart endpoint to project endpoints

--- shop/urls.py
+++ shop/urls.py
@@ -23,6 +23,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("accounts/", include("allauth.urls")),
+    path("cart/", include("cart.urls", namespace="cart")),
     path("", include("core.urls", namespace="core")),
 ]
 
